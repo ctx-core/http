18b5c79ad16b6aded0209ec5b079e639edada0f9
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/assign_http_headers.js
+++
@@ -1,9 +0,0 @@
-import { assign } from '@ctx-core/object';
-export function assign_http_headers(ctx, ...header_a) {
-    const headers = ctx.headers || {};
-    assign(headers, ...header_a);
-    ctx.headers = headers;
-    return ctx;
-}
-export { assign_http_headers as assign__headers__http };
-//# sourceMappingURL=src/assign_http_headers.js.map
